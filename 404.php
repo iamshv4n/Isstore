4e5c043eb91d7d7e70be891ec179ccf6b06160a9
Omko00087 <131091310+Omko00087@users.noreply.github.com>
2023-08-24 14:36:50 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,50 @@
+<html lang="ku">
+<head><meta charset="utf-8">
+    
+    <!-- Halfmoon CSS -->
+<link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
+<!--
+  Or,
+  Use the following (no variables, supports IE11):
+  <link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon.min.css" rel="stylesheet" />
+-->
+
+<!-- Halfmoon JS -->
+<script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/js/halfmoon.min.js"></script>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+</head>
+<body>
+
+  <p class="text1"> 404 | Not Found </p>
+  <p class="text2"> ئەم پەڕەیە نەدۆزرایەوە </p>
+<center>
+
+   <a href="/app"> <button class="btn btn-primary" type="button">home</button></a>
+
+</center>
+  <style>
+
+.text1{
+padding-top: 250px;
+    font-size: 30px;
+    text-align: center;
+
+}
+
+.text2{
+    font-size: 20px;
+    text-align: center;
+    font-family: kurds;
+
+}
+
+@font-face {
+  font-family: kurds;
+  src: url(assets/fonts/MaterialIcons-Regular.ttf);
+
+}
+  </style>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="ku">
+    <head>
+
+<meta name="description" content="store for plus apps ، وێبسایتێکی کوردی بۆ بەرنامە پڵاسەکان">
+<meta name="keywords" content="unc0ver, Odyssey, chmira jailbreak, Videostar++, app++, cydia, ڤیدۆستار ++، سیدیا،">
+<meta name="author" content="sardam">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="white">
+    <link rel="shortcut icon" href="https://sardam12333.000webhostapp.com/icon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="https://sardam12333.000webhostapp.com/icon.ico">
+    <link rel="apple-touch-icon" sizes="72x72" href="https://sardam12333.000webhostapp.com/icon.ico">
+    <link rel="apple-touch-icon" sizes="114x114" href="https://sardam12333.000webhostapp.com/icon.ico">
+    <title>sardam story/سەردەم ستوری</title>
+    <meta charset="UTF-8">
+
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
+<link href="dl/css/bootstrap.min.css" rel="stylesheet">
+<link href="dl/css/style.css" rel="stylesheet">
+
+
+
+</head><body style="background-color:#121212 ;">
+
+<link href="dl/style.css" rel="stylesheet">
+
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800" rel="stylesheet">
+
+
+
+
+
+<header id="mu-header" class="" role="banner">
+<div class="mu-header-overlay">
+<div class="container">
+<div class="mu-header-area">
+
+<div class="mu-logo-area">
+
+<a class="mu-logo" href="snapchat://add/sardam-story">sardam<br>story</a>
+</div>
+
+<div class="mu-header-featured-area">
+<div class="mu-header-featured-img">
+</div>
+
+
+
+<div class="mu-header-featured-content">
+<h1>بەخیربی بو  <br> <span>سەردەم ستوری</span></h1>
+<img src="dl/img/logo.ico" alt="sardamstory">
+<p>باشترین ستورەکان بو سیستەمی ئایفون</p>
+
+     <a href="https://www.instagram.com/sardam_sadq.story/">
+<img alt="Qries" src="dl/img/ins.ico" width="190&quot;" height="190">
+<br>
+<br>
+<br>
+</a>
+<a class="mu-apple-btn"  href="/AppFiles/webclip.mobileconfig"> <i class="fa fa-apple"></i><span>دابەزاندن</span></a>
+<br>
+<br>
+<a class="mu-apple-btn" href="/app"><i class="fa fa-apple"></i><span>بێ پرۆفایل</span></a>
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+
+</header>
+
+
+<main role="main">
+</main>
+
+
+<footer id="mu-footer" role="contentinfo">
+<div class="container">
+<div class="mu-footer-area">
+<p class="mu-copy-right">© Sardam Sadq . All right reserved.</p>
+</div>
+</div>
+</footer>
+
+<!--
+<body>
+    <br><br><br>
+    <div class="stars"></div>
+    <div class="twinkling"></div>
+    <div class="galaxy"></div>
+    <div class="lamps-container">
+        <div class="lamps">
+            <img class="vert-move1" src="dl/images/2.png" />
+            <img class="vert-move3" src="dl/images/4.png" />
+            <img class="vert-move2" src="dl/images/3.png" />
+        </div>
+        <div class="lamps-sec">
+            <img class="vert-move1" src="dl/images/2.png" />
+            <img class="vert-move3" src="dl/images/4.png" />
+            <img class="vert-move2" src="dl/images/3.png" />
+        </div>
+    
+    <script>
+        // Random Stars
+        var generateStars = function () {
+
+            var $galaxy = $(".galaxy");
+            var iterator = 0;
+
+            while (iterator <= 100) {
+                var xposition = Math.random();
+                var yposition = Math.random();
+                var star_type = Math.floor((Math.random() * 3) + 1);
+                var position = {
+                    "x": $galaxy.width() * xposition,
+                    "y": $galaxy.height() * yposition,
+                };
+
+                $('<div class="star star-type' + star_type + '"></div>').appendTo($galaxy).css({
+                    "top": position.y,
+                    "left": position.x
+                });
+
+                iterator++;
+            }
+
+        };
+
+        generateStars();
+    </script>
+-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        
+
+
+<script src="/dl/js/bootstrap.min.js"></script>
+
+
+<script type="text/javascript" src="/dl/js/app.js"></script>
+
+
+<script src="dl/js/en_script.min.js"></script>
+
+
+
+</html>
